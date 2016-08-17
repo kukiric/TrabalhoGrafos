794e32fea6ac713ad993c8c84c864347da5debbe
Ricardo Maes <ricmzn@gmail.com>
2016-08-17 17:13:35 -0300

    Adicionados typings e grafo de teste

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import xml2js = require("xml2js");
+import util = require("util");
+import fs = require("fs");
 
 class Arco {
     public id: Number;
@@ -14,11 +16,17 @@ class No {
 class Grafo {
     public nos: No[];
     public arcos: Arco[];
+
+    public static ImportaGrafo(caminho: string): Grafo {
+        let arquivo = fs.readFileSync(caminho);
+        xml2js.parseString(arquivo.toString(), function(err, result) {
+            if (err != null) {
+                console.error("Erro na leitura do XML: " + err);
+            }
+        });
+        return null;
+    }
 }
 
 console.log("Importando XML...");
-xml2js.parseString("<elemento><valor>1.5</valor><valor>2.3</valor></elemento>", function(err, result) {
-    console.log("Imprimindo:");
-    console.log(result);
-    console.log(result.elemento.valor);
-});
+let grafo = Grafo.ImportaGrafo("grafo1.xml");
