db8a0aa9f1a7f387d9720fb938d24f741d97ce26
Ricardo Maes <ricmzn@gmail.com>
2016-08-24 16:20:04 -0300

    Removida distorção do canvas e adicionado botão DFS

--- view/index.js
+++ view/index.js
@@ -1,13 +1,21 @@
 const electron = require("electron");
 const grafos = require("../src/grafos");
 
-const canvas = document.getElementById("grafo").getContext("2d");
+const contexto = document.getElementById("grafo").getContext("2d");
+let grafo;
+
+window.addEventListener("resize", (event) => {
+    if (grafo != null) {
+        desenhaGrafo(grafo);
+    }
+});
 
 function abrirGrafo() {
     electron.ipcRenderer.once("set-grafo", (evento, grafoAciclico) => {
         if (grafoAciclico != null) {
             grafoAciclico.toGrafo = grafos.GrafoAciclico.prototype.toGrafo;
-            desenhaGrafo(grafoAciclico.toGrafo());
+            grafo = grafoAciclico.toGrafo();
+            desenhaGrafo(grafo);
         }
     });
     electron.ipcRenderer.send("abrir-grafo", "set-grafo");
@@ -17,27 +25,38 @@ function devTools() {
     electron.ipcRenderer.send('dev-tools');
 }
 
+function DFS() {
+    if (grafo != null) {
+        let v1 = grafo.getVerticePorNome("A");
+        let encontrados = grafos.buscaDFS(v1);
+        let lista = encontrados.map((vertice) => vertice.nome).join(", ");
+        alert("Vértices encontrados a partir de A: [" + lista + "]");
+    }
+}
+
 function desenhaGrafo(grafo) {
-    canvas.clearRect(0, 0, 1024, 768);
+    contexto.canvas.width = contexto.canvas.scrollWidth;
+    contexto.canvas.height = contexto.canvas.scrollHeight;
+    contexto.clearRect(0, 0, contexto.canvas.width, contexto.canvas.height);
     function drawVertice(pos, nome) {
-        canvas.beginPath();
-        canvas.lineWidth = 4;
-        canvas.arc(pos.x, pos.y, 16, 0, 2*Math.PI);
-        canvas.stroke();
-        canvas.fillStyle = "yellow";
-        canvas.arc(pos.x, pos.y, 16, 0, 2*Math.PI);
-        canvas.fill();
-        canvas.fillStyle = "black";
-        canvas.textAlign = "center";
-        canvas.font = "12px sans-serif";
-        canvas.fillText(nome, pos.x, pos.y + 4);
+        contexto.beginPath();
+        contexto.lineWidth = 4;
+        contexto.arc(pos.x, pos.y, 16, 0, 2*Math.PI);
+        contexto.stroke();
+        contexto.fillStyle = "yellow";
+        contexto.arc(pos.x, pos.y, 16, 0, 2*Math.PI);
+        contexto.fill();
+        contexto.fillStyle = "black";
+        contexto.textAlign = "center";
+        contexto.font = "12px sans-serif";
+        contexto.fillText(nome, pos.x, pos.y + 4);
     }
     function drawArco(pos1, pos2) {
-        canvas.beginPath();
-        canvas.lineWidth = 2;
-        canvas.moveTo(pos1.x, pos1.y);
-        canvas.lineTo(pos2.x, pos2.y);
-        canvas.stroke();
+        contexto.beginPath();
+        contexto.lineWidth = 2;
+        contexto.moveTo(pos1.x, pos1.y);
+        contexto.lineTo(pos2.x, pos2.y);
+        contexto.stroke();
     }
     // Desenha os arcos primeiro
     grafo.vertices.forEach((vertice) => {
