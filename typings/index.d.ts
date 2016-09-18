6060f1f1a86a9b22e4f4eb7b50ab9398e52b6f87
Ricardo Maes <ricmzn@gmail.com>
2016-09-18 05:27:54 -0300

    Adicionados typings do Materialize no projeto

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/materialize-css/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/xml2js/index.d.ts" />
