260dfc03ad85754e1e66db0716a2234881d8fd90
Ricardo Maes <ricmzn@gmail.com>
2016-09-20 21:08:04 -0300

    Removidos typings globais do projeto e adicionado typings.json no lugar

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/github-electron/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/materialize-css/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/xml2js/index.d.ts" />
